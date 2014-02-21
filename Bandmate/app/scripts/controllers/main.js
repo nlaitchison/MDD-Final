69dac97a85e28c46fe578b3c67aa7f21b0b81bfb
Nicole Aitchison <nlaitchison@gmail.com>
2014-02-20 23:40:21 -0500

    remade the project

---
+++ Bandmate/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bandmateApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Bandmate/app/scripts/services/factory.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* global Firebase */
+angular.module('bandmateApp')
+	.factory('FireConn', ['$firebase',function ($firebase) {
+
+		var url = 'https://bandmate.firebaseIO.com',
+			ref = new Firebase(url);
+
+		return $firebase(ref);
+	}]);
